747acef840040375c9eb36515e640f5a9afc1bd5
Marioluigi2002 <birka016@outlook.com>
2022-05-12 13:01:06 +0100

    Backend Test

---
+++ A_Test.js
@@ -0,0 +1,11 @@
+function CreateHTMLProjectElements(){
+
+
+ // Project number: 0
+ //Element 0
+ // Text Component
+ var portolioElemet = document.getElementById("GEC_2_Text");
+ portolioElemet.innerHTML = "A Mario game created in C++ using SDL libary.";
+
+}
+
